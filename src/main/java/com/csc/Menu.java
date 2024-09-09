35a4bf3d20645bf81243330d3482accac0cf5287
verystellar <161178496+verystellar@users.noreply.github.com>
2024-09-09 14:24:16 -0500

    menu implemented

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -1,3 +1,33 @@
 package com.csc;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 // Your code goes here!
+public class Menu{
+    
+    public int callMenu(int b1, int b2){
+        Scanner newScanner = new Scanner(System.in);
+        boolean val = false;
+        int num = 0;
+        while(val != true){
+        System.out.println("Enter number between (" + b1 + "," + b2 + "): ");
+        try { 
+            String boo = newScanner.nextLine();
+            int i = Integer.valueOf(boo);
+            if (i < b1 || i > b2) {
+                System.out.println("Value out of bounds\n");
+                
+            } else {
+                val = true;
+                System.out.println("Value chosen by user was " + i);
+                num = i;
+            }
+            }
+        
+        catch (NumberFormatException ex){
+            System.out.println("Invalid Value\n");
+        }
+        }
+        return num;
+    }
+}
+
---
+++ src/main/java/com/driver/Driver.java
@@ -0,0 +1,11 @@
+import com.csc.Menu;
+
+public class Driver {
+    public static void main(String[] args) {
+        Menu myMenu = new Menu();
+        myMenu.callMenu(1, 100);
+        myMenu.callMenu(-10, 10);
+    }
+
+    
+}
